8bb55747fa73f92c11897f43fd1030a997aaf5c1
ArturT <arturtrzop@gmail.com>
2016-03-21 18:37:16 +0100

    Add logger
    Example for iex -S mix
    Issues.CLI.process {"ArturT", "knapsack", 1}

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,9 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   # use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
   @user_agent [ {"User-agent", "Elixir"} ]
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -14,11 +17,14 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     # TODO use exjsx
     { :ok, :jsx.decode(body) }
   end
 
-  def handle_response({_, %HTTPoison.Response{status_code: _, body: body}}) do
+  def handle_response({_, %HTTPoison.Response{status_code: status, body: body}}) do
+    Logger.error "Error #{status} returned"
     { :error, :jsx.decode(body) }
   end
 end

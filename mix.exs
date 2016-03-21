6d65c9809a07465de6ee424de9ee3423e15fdde1
ArturT <arturtrzop@gmail.com>
2016-03-21 19:27:44 +0100

    Add project name and source url in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Issues.Mixfile do
   def project do
     [app: :issues,
      version: "0.0.1",
+     name: "Issues",
+     source_url: "https://github.com/ArturT/elixir-issues",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
